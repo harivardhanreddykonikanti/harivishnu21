dd4524b46c4733c0493ac710c3bf20a005be0a1f
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2025-03-11 12:27:53 +0530

    Even Number

---
+++ SamplePrograms/src/SamplePrograms/EvenNumber.java
@@ -0,0 +1,14 @@
+package SamplePrograms;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		int num=4;
+         if(num%2==0) {
+        	 System.out.println("Even Number");
+         }else {
+        	 System.out.println("Odd Number");
+         }
+	}
+
+}
