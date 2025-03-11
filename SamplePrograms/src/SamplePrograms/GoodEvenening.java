3c036fbef1074b2e35b56e61fcb29ba19db07be6
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2025-03-11 17:13:13 +0530

    Good  Evening created by vishnu

---
+++ SamplePrograms/src/SamplePrograms/GoodEvenening.java
@@ -0,0 +1,10 @@
+package SamplePrograms;
+
+public class GoodEvenening {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            System.out.println("Good evening");
+	}
+
+}
