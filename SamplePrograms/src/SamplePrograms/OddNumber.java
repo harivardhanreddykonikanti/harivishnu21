8d6bccc79025c8556b02b204463cdfc792724a98
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2025-03-11 13:21:12 +0530

    Merge branch 'main' of https://github.com/harivardhanreddykonikanti/harivishnu21.git into main

---
+++ SamplePrograms/src/SamplePrograms/OddNumber.java
@@ -0,0 +1,15 @@
+package SamplePrograms;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		
+       int num=5;
+       if(num%2==1) {
+    	   System.out.println("Odd Number");
+       }else {
+    	   System.out.println("Not a odd Number");
+       }
+	}
+
+}
