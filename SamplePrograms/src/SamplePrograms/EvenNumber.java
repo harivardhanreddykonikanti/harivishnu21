46b8b341c1824ca94ade8dc330be51af41d366fd
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2025-03-11 12:30:39 +0530

    modifications

--- SamplePrograms/src/SamplePrograms/EvenNumber.java
+++ SamplePrograms/src/SamplePrograms/EvenNumber.java
@@ -5,7 +5,7 @@ public class EvenNumber {
 	public static void main(String[] args) {
 		int num=4;
          if(num%2==0) {
-        	 System.out.println("Even Number");
+        	 System.out.println("yes it is Even Number");
          }else {
         	 System.out.println("Odd Number");
          }
